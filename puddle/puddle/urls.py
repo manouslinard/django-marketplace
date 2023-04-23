ffc2344257330eed090ac2c73232a5911f5e3f81
manouslinard <it22064@hua.gr>
2023-04-23 19:25:18 +0300

    added signup

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -2,13 +2,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from core.views import index, contact
 from item import urls as item_urls
 
 
 urlpatterns = [
-    path("", index, name="index"),
+    path("", include('core.urls')),
     path("items/", include(item_urls)),
-    path("contact/", contact, name="contact"),
     path("admin/", admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
